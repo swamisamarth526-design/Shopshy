98540e033645f64868c9680efbed4d2d7ec3f27d
swamisamarth526-design <swamisamarth526@gmail.com>
2026-04-17 13:21:08 +0530

    Add Mangodb

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,13 @@ const mongoose = require("mongoose")
 mongoose.set('strictQuery', false)
 
 const connectDB = async () => {
+    const uri = process.env.MONGO_URI
+    if (!uri) {
+        console.error("Error: MONGO_URI is not defined. Set it in server/.env or Railway environment variables.")
+        process.exit(1)
+    }
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(uri)
         console.log("Mongo DB Connected: ", conn.connection.host)
     }
     catch(err) {
