0294a4a92b0626a8e38d25202d7710917e6b935a
swamisamarth526-design <swamisamarth526@gmail.com>
2026-04-17 13:37:37 +0530

    Add Mangodb

--- server/server.js
+++ server/server.js
@@ -2,7 +2,17 @@ const path = require("path")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-require("dotenv").config()
+
+const envPath = path.resolve(__dirname, ".env")
+const rootEnvPath = path.resolve(__dirname, "..", ".env")
+if (require("fs").existsSync(envPath)) {
+  require("dotenv").config({ path: envPath })
+} else if (require("fs").existsSync(rootEnvPath)) {
+  require("dotenv").config({ path: rootEnvPath })
+} else {
+  require("dotenv").config()
+}
+
 const connectDB = require("./config/db")
 const PORT = process.env.PORT || 5000
 
